74c90d851cb4c3e377a3cad0bdce2fa825d73f87
Arpit Nigam <arpit.nigam@outlook.com>
2020-04-11 11:06:16 -0700

    Updates

---
+++ precache-manifest.d8d865dfa2fbb59ce72da3887dab77c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b04ccf8ee00eb4873be81529f8083a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8cd02fa51537f272c22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50a839ceb2558d977d00",
+    "url": "/static/js/2.07cebffd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07cebffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cd02fa51537f272c22",
+    "url": "/static/js/main.4094d59d.chunk.js"
+  },
+  {
+    "revision": "ab8abf84156eed815586",
+    "url": "/static/js/runtime-main.3a186c54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
